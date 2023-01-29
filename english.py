339e4359b9527296845d19d7ce1197d4400f7fc4
Shivam Kumar <114799228+kumarshivam27december@users.noreply.github.com>
2023-01-29 12:33:38 +0530

    Update english.py

--- english.py
+++ english.py
@@ -1,5 +1,5 @@
 # although this project does not work properly but still it can help to build basic of how this kind of projects are made with the help of python language
-
+# the major problem with this type of code is that it will not give the most expected output like it can give very basic correction not very advance grammer
 # # from textblob import TextBlob
 # # from textwo
 # # import textblob
